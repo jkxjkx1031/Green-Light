3f6764b0f5916ae6097597c290dee182bb093c6c
jkxjkx1031 <jkxjkx1031@163.com>
2019-02-09 11:08:09 +0800

    Models initialized.

--- trade/models.py
+++ trade/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=20)
+    passwd = models.CharField(max_length=100)
+    asset = models.IntegerField(default=0)
+    credit = models.IntegerField(default=100)
+    isIndividual = models.BooleanField(default=True)
+    isValidated = models.BooleanField(default=False)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    Price = models.IntegerField()
+    isValidated = models.BooleanField(default=False)
